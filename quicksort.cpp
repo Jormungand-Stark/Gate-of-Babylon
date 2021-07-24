906aabdb8dd17ba629201fca72588f6f95c4ee05
XIANG-LEE <71392262+Jormungand-Stark@users.noreply.github.com>
2021-07-24 13:02:13 +0800

    Create quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,3 +1,4 @@
+//flag参与置换
 void quicksort(vector<int>& arr, int left, int right){
   if(left > right){
     return;
@@ -16,6 +17,29 @@ void quicksort(vector<int>& arr, int left, int right){
     }
     swap(arr[i], arr[j]);
   }
+  quicksort(arr, left, i-1);
+  quicksort(arr, i+1, right);
+}
+
+//另一种，flag不参与置换，最后归位flag。
+void  quicksort ( vector < int >&  arr , int  left , int  right ){
+  if(left > right){
+    return;
+  }
+  int i = left;
+  int j = right;
+        
+  int flag = arr[i];
+  while(i!=j){
+    while(arr[j]>=flag && i<j){
+      j--;
+    }
+    while(arr[i]<=flag && i<j){
+      i++;
+    }
+    swap(arr[i], arr[j]);
+  }
+  swap(arr[i], arr[left]);
   quicksort(arr, left, i-1);
   quicksort(arr, i+1, right);
 }
